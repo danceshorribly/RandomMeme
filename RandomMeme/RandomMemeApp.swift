72afca9d5bee0ac02b276111332c3d4995df76e1
Oscar Byhlinder <oscar@byhlinder.com>
2023-12-14 12:11:10 +0100

    Initial Commit

---
+++ RandomMeme/RandomMemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomMemeApp.swift
+//  RandomMeme
+//
+//  Created by Oscar Byhlinder on 2023-12-14.
+//
+
+import SwiftUI
+
+@main
+struct RandomMemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
